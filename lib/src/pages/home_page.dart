f088ebd9cc4c8a554b10cf11cd9400e0b03fe640
Jan Carlo Guzman Sanchez <al221910462@gmail.com>
2020-12-01 01:53:23 +0100

    Dos videos

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:qr/src/pages/direcciones_page.dart';
+import 'package:qr/src/pages/mapas_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _callPage(currentIndex),
+        bottomNavigationBar: _craerBottomNavigationBar());
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
+  }
+
+  Widget _craerBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.brightness_5), title: Text('Direcciones')),
+      ],
+    );
+  }
+}
